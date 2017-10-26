570bb07401c352d9dcbdb50a4b8abc01b96cd7d1
Denny Fauzan <dennyfauzan92@gmail.com>
2017-10-26 17:52:24 +0700

    Add PATCH /todos/:id

--- server/server.js
+++ server/server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const _ = require('lodash');
 const bodyParser = require('body-parser');
 const {ObjectID} = require('mongodb');
 var {mongoose} = require('./db/mongoose');
@@ -67,6 +68,34 @@ app.delete('/todos/:id', (req, res) => {
   })
 
 });
+
+app.patch('/todos/:id', (req, res) => {
+  var id = req.params.id;
+  var body = _.pick(req.body, ['text', 'completed']);
+
+  if(!ObjectID.isValid(id)){
+    return res.status(404).send();
+  }
+
+  if(_.isBoolean(body.completed) && body.completed ){
+    body.completedAt = new Date().getTime();
+  } else {
+    body.completedAt = false;
+    body.completedAt = null;
+  }
+
+  Todo.findByIdAndUpdate(id, {$set: body}, {new: true}).then((todo) => {
+    if(!todo){
+      return res.status(404).send();
+    }
+    
+    res.send({todo});
+  })
+  .catch((e) => {
+    res.status(400).send();
+  })
+
+})
 
 app.listen(port, () => {
   console.log(`Server Up to port ${port}`);
