b949f2e683b052fb6b9377bda5333fbae863f402
Denny Fauzan <dennyfauzan92@gmail.com>
2017-10-26 19:01:57 +0700

    Set config condition to connect between mLab and heroku

--- server/config/config.js
+++ server/config/config.js
@@ -6,4 +6,6 @@ if(env === 'development'){
 } else if(env === 'test'){
   process.env.PORT = 3000;
   process.env.MONGODB_URI = 'mongodb://localhost:27017/TodoAppTest' ;
+} else {
+  process.env.MONGODB_URI = 'mongodb://admin:admin@ds233895.mlab.com:33895/node-todo-api';
 }
