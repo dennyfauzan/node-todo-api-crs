a92878cca3fe8264f829f81b9f8aa85a07683a91
Denny Fauzan <dennyfauzan92@gmail.com>
2017-10-26 17:57:43 +0700

    Connecting with mLab database

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,9 +1,9 @@
 const mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost:27017/TodoApp');
+// mongoose.connect('mongodb://localhost:27017/TodoApp');
 
 //Db connected via mLab
-// mongoose.connect('mongodb://dennyfauzan:mongolab0901@ds233895.mlab.com:33895/node-todo-api')
+mongoose.connect('mongodb://admin:admin@ds233895.mlab.com:33895/node-todo-api');
 
 module.exports = {
   mongoose
